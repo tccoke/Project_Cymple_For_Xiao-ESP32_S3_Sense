1eca3c0027848691a5c3deac5d0028263ea8deec
ge90114b <111622870+ge90114b@users.noreply.github.com>
2024-10-19 11:03:34 +0800

    Update camCfg.h

--- eyeTracker/software/src/camCfg.h
+++ eyeTracker/software/src/camCfg.h
@@ -2,24 +2,42 @@
 #define _CAM_PINS_DEF_H_
 #include <esp_camera.h>
 #include "config.h"
+#define PWDN_GPIO_NUM -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 15
+#define SIOD_GPIO_NUM 4
+#define SIOC_GPIO_NUM 5
+
+#define Y2_GPIO_NUM 11
+#define Y3_GPIO_NUM 9
+#define Y4_GPIO_NUM 8
+#define Y5_GPIO_NUM 10
+#define Y6_GPIO_NUM 12
+#define Y7_GPIO_NUM 18
+#define Y8_GPIO_NUM 17
+#define Y9_GPIO_NUM 16
+
+#define VSYNC_GPIO_NUM 6
+#define HREF_GPIO_NUM 7
+#define PCLK_GPIO_NUM 13
 /** @brief Pin definition for AI Thinker ESP32-CAM. */
 constexpr camera_config_t camconfig{
-  .pin_pwdn = 32,
+  .pin_pwdn = PWDN_GPIO_NUM,
   .pin_reset = -1,
-  .pin_xclk = 0,
-  .pin_sccb_sda = 26,
-  .pin_sccb_scl = 27,
-  .pin_d7 = 35,
-  .pin_d6 = 34,
-  .pin_d5 = 39,
-  .pin_d4 = 36,
-  .pin_d3 = 21,
-  .pin_d2 = 19,
-  .pin_d1 = 18,
-  .pin_d0 = 5,
-  .pin_vsync = 25,
-  .pin_href = 23,
-  .pin_pclk = 22,
+  .pin_xclk = XCLK_GPIO_NUM,
+  .pin_sccb_sda = SIOD_GPIO_NUM,
+  .pin_sccb_scl = SIOC_GPIO_NUM,
+  .pin_d7 = Y9_GPIO_NUM,
+  .pin_d6 = Y8_GPIO_NUM,
+  .pin_d5 = Y7_GPIO_NUM,
+  .pin_d4 = Y6_GPIO_NUM,
+  .pin_d3 = Y5_GPIO_NUM,
+  .pin_d2 = Y4_GPIO_NUM,
+  .pin_d1 = Y3_GPIO_NUM,
+  .pin_d0 = Y2_GPIO_NUM,
+  .pin_vsync = VSYNC_GPIO_NUM,
+  .pin_href = HREF_GPIO_NUM,
+  .pin_pclk = PCLK_GPIO_NUM,
     //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
   .xclk_freq_hz = 20000000,
   .ledc_timer = LEDC_TIMER_0,
